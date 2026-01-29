2f84334bffd7479d2fea1fddb8df7a928e04c01c
Eduardo Ranzan Ferreira <ranzaneduardo03@gmail.com>
2026-01-29 16:41:02 -0300

    feat: first widget

--- lib/widgets/emoji_button_widget.dart
+++ lib/widgets/emoji_button_widget.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_labs/widgets/shared/app_bar_widget.dart';
 
@@ -10,17 +9,106 @@ class EmojiButton extends StatefulWidget{
 }
 
 class _EmojiButtonState extends State<EmojiButton> {
+  IconData? _iconPressed;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
-      body: _buildBody(),
+      body: Padding(
+          padding: const EdgeInsetsGeometry.all(15),
+          child: Center(
+              child: _buildBody(),
+          )
+      ),
     );
   }
 
   Widget _buildBody() {
-    return Container(
+    return Row(
+      spacing: 10,
+      children: [
+        Expanded(
+            child: EmojiButtonBuild(
+              icon: Icons.sentiment_satisfied,
+              isSelected: _iconPressed == Icons.sentiment_satisfied,
+              color: Colors.green,
+              onTap: () => _onEmojiTap(Icons.sentiment_satisfied),
+            )
+        ),
+        Expanded(
+            child: EmojiButtonBuild(
+              icon: Icons.sentiment_neutral,
+              isSelected: _iconPressed == Icons.sentiment_neutral,
+              color: Colors.yellow,
+              onTap: () => _onEmojiTap(Icons.sentiment_neutral),
+            )
+        ),
+        Expanded(
+            child: EmojiButtonBuild(
+              icon: Icons.sentiment_dissatisfied,
+              isSelected: _iconPressed == Icons.sentiment_dissatisfied,
+              color: Colors.red,
+              onTap: () => _onEmojiTap(Icons.sentiment_dissatisfied),
+            )
+        ),
+      ],
+    );
+  }
 
+  void _onEmojiTap(IconData icon) {
+    setState(() {
+      if (_iconPressed == icon) {
+        _iconPressed = null;
+      } else {
+        _iconPressed = icon;
+      }
+    });
+  }
+}
+
+class EmojiButtonBuild extends StatelessWidget {
+  final IconData icon;
+  final bool isSelected;
+  final Color color;
+  final VoidCallback onTap;
+
+  const EmojiButtonBuild({
+    super.key,
+    required this.icon,
+    required this.isSelected,
+    required this.color,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeOut,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        decoration: BoxDecoration(
+          color: isSelected ? color : Colors.grey,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: isSelected
+              ? [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            )
+          ]
+              : [],
+        ),
+        child: Icon(
+          icon,
+          size: 40,
+          color: isSelected ? Colors.white : Colors.black,
+        ),
+      ),
     );
   }
 }
